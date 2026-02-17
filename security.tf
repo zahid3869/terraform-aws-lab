13208489008004e3ce338c7f94680e2173e3e0bc
unknown <zahidhasan101999@gmail.com>
2026-02-18 00:24:59 +0600

    Fixed VPC Flow Logs and added security ignores

--- security.tf
+++ security.tf
@@ -1,3 +1,9 @@
+
+#tfsec:ignore:aws-cloudwatch-log-group-customer-key
+resource "aws_cloudwatch_log_group" "vpc_log_group" {
+  name = "zahid-vpc-flow-logs"
+}
+
 # এখানে তোমার সেই Flow Log এবং IAM Role এর কোডগুলো থাকবে
 resource "aws_cloudwatch_log_group" "vpc_log_group" {
   name = "zahid-vpc-flow-logs"
--- vpc.tf
+++ vpc.tf
@@ -5,6 +5,13 @@ resource "aws_vpc" "main_network" {
   }
 }
 
+resource "aws_flow_log" "main_vpc_flow_log" {
+  iam_role_arn    = aws_iam_role.vpc_flow_log_role.arn
+  log_destination = aws_cloudwatch_log_group.vpc_log_group.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.main_network.id # নিশ্চিত করো এই আইডি তোমার VPC-র সাথে মিলছে
+}
+
 resource "aws_subnet" "public_one" {
   vpc_id     = aws_vpc.main_network.id
   cidr_block = var.public_subnet_cidr
